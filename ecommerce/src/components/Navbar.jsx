08df36336aa231dd867f411a9a6b6283289ad856
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-09-16 22:38:01 +0800

    Navbar

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,29 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
 
 
 const Container = styled.div`
     height: 60 px;
-`
+`;
 
 const Wrapper = styled.div`
     padding: 10px 20px ;
     display: flex;
+    align-items: center;
     justify-content: space-between;
-`
+`;
+
 const Left = styled.div`
     flex: 1;
-`
+    display: flex;
+    align-items: center;
+`;
+
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
-`
+`;
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+
+
 const Center = styled.div`
     flex: 1;
-`
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
 const Right = styled.div`
     flex: 1;
-`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+const MenuItems = styled.div`
+    font-size: 14px;
+    cursor: pointer; 
+    margin-left: 25px
+`;
+
 
 const Navbar = () => {
     return (
@@ -31,12 +68,26 @@ const Navbar = () => {
             <Wrapper>
                 <Left>
                     <Language>EN</Language>
+                    <SearchContainer>
+                        <Input />
+                        <Search />
+                    </SearchContainer>
                 </Left>
-                <Center>center</Center>
-                <Right>right</Right>
+                <Center>
+                    <Logo>MightyJaxx</Logo>
+                </Center>
+                <Right>
+                    <MenuItems>REGISTER</MenuItems>
+                    <MenuItems>SIGN IN</MenuItems>
+                    <MenuItems>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItems>
+                </Right>
             </Wrapper>
         </Container>
-    )
-}
+    );
+};
 
 export default Navbar
