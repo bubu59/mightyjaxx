e0def3d8988e6a5d33601e685fd125ae782e4474
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-09-18 11:11:45 +0800

    Newsletter

--- ecommerce/src/data.js
+++ ecommerce/src/data.js
@@ -43,34 +43,34 @@ export const categories = [
 export const popularProducts = [
     {
         id: 1,
-        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+        img: "https://images.fun.com/products/77912/1-2/mcfarlane-batman-black-and-white-by-brian-bolland.jpg",
     },
     {
         id: 2,
-        img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+        img: "https://images.fun.com/products/77917/1-2/mcfarlane-dc-cover-girls-poison-ivy-by-frank-cho-s.jpg",
     },
     {
         id: 3,
-        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+        img: "https://images.fun.com/products/77920/1-2/marvel-premier-collection-x-men-action-bishop-statue.jpg",
     },
     {
         id: 4,
-        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+        img: "https://images.fun.com/products/84638/1-2/pop-marvel-thor-love-and-thunder--the-goat-boat.jpg",
     },
     {
         id: 5,
-        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+        img: "https://images.fun.com/products/71809/1-2/funko-pop-deluxe-marvel-heroes-king-deadpool-on-throne.jpg",
     },
     {
         id: 6,
-        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+        img: "https://images.fun.com/products/77299/1-2/diamond-select-marvel-animated-doctor-octopus-bust.jpg",
     },
     {
         id: 7,
-        img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+        img: "https://images.fun.com/products/84641/1-2/pop-marvel-moon-knight--mr-knight.jpg",
     },
     {
         id: 8,
-        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+        img: "https://images.fun.com/products/84644/1-2/pop-marvel-moon-knight-arthur-harrow.jpg",
     },
 ]
