98243c9b527f0ad76d04acf7d56d7837489b75be
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-09-19 20:24:20 +0800

    cart, order, product routes

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 
 dotenv.config();
@@ -18,6 +20,8 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
