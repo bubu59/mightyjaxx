2f0e1ad3f49bbd965a654f339c88f987601ebdb5
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-09-19 11:22:54 +0800

    setup MongoDB, connect to MongoDB, use DOTENV

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection successful"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/api/test", () => {
+    console.log("test is successful")
+});
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
 });
