3301809a6939642a96612c17b1ee3bed0d607d90
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-09-24 21:04:33 +0800

    product form

--- admin/src/pages/Product.jsx
+++ admin/src/pages/Product.jsx
@@ -1,39 +1,139 @@
+import { Publish } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import productRedux from "../redux/productRedux";
 
 
-const Container = styled.div``;
+const Container = styled.div`
+    flex: 4;
+    padding: 20px;
+`;
 
-const ProductTitleContainer = styled.div``;
+const ProductTitleContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
 
 const ProductTitle = styled.h1``;
 
 
-const ProductAddButton = styled.button``;
+const ProductAddButton = styled.button`
+    width: 80px;
+    border: none;
+    padding: 5px;
+    background-color: teal;
+    color: white;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+`;
 
-const ProductTop = styled.div``;
+const ProductTop = styled.div`
+    display: flex;
+`;
 
-const ProductTopLeft = styled.div``;
+const ProductTopLeft = styled.div`
+    flex:1;
+`;
 
-const ProductTopRight = styled.div``;
+const ProductTopRight = styled.div`
+    flex:1;
+    padding: 20px;
+    margin: 20px;
+    -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`;
 
-const ProductInfoTop = styled.div``;
+const ProductInfoTop = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
-const ProductInfoImg = styled.img``;
+const ProductInfoImg = styled.img`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-right: 20px;
+`;
 
-const ProductName = styled.span``;
+const ProductName = styled.span`
+    font-weight: 600;
+`;
 
-const ProductInfoBottom = styled.div``;
+const ProductInfoBottom = styled.div`
+    margin-top: 10px;
+`;
 
-const ProductInfoItem = styled.div``;
+const ProductInfoItem = styled.div`
+    width: 150px;
+    display: flex;
+    justify-content: space-between;
+`;
 
 const ProductInfoKey = styled.span``;
 
-const ProductInfoValue = styled.span``;
+const ProductInfoValue = styled.span`
+    font-weight: 300;
+`;
 
-const ProductBottom = styled.div``;
+const ProductBottom = styled.div`
+    padding: 20px;
+    margin: 20px;
+    -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`;
 
+const ProductForm = styled.form`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const ProductFormLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Label = styled.label`
+    margin-bottom: 10px;
+    color: gray;
+`;
+
+const Input = styled.input`
+    margin-bottom: 10px;
+    border: none;
+    padding: 5px;
+    border-bottom: 1px solid gray;
+`;
+
+const Select = styled.select`
+    margin-bottom: 10px;
+`;
+
+const Option = styled.option``;
+
+const ProductFormRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const ProductUpload = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const ProductUploadImg = styled.img``;
+
+const ProductButton = styled.button`
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+    background-color: darkblue;
+    color:white;
+    font-weight: 600;
+    cursor: pointer;
+`;
 
 
 const Product = () => {
@@ -50,7 +150,7 @@ const Product = () => {
             </ProductTitleContainer>
             <ProductTop>
                 <ProductTopLeft>
-
+                    Chart
                 </ProductTopLeft>
                 <ProductTopRight>
                     <ProductInfoTop>
@@ -58,13 +158,47 @@ const Product = () => {
                         <ProductName>BUBU</ProductName>
                     </ProductInfoTop>
                     <ProductInfoBottom>
-                        <ProductInfoKey>id:</ProductInfoKey>
-                        <ProductInfoValue>123456</ProductInfoValue>
+                        <ProductInfoItem>
+                            <ProductInfoKey>id:</ProductInfoKey>
+                            <ProductInfoValue>123456</ProductInfoValue>
+                        </ProductInfoItem>
+                        <ProductInfoItem>
+                            <ProductInfoKey>sales:</ProductInfoKey>
+                            <ProductInfoValue>5123</ProductInfoValue>
+                        </ProductInfoItem>
+                        <ProductInfoItem>
+                            <ProductInfoKey>in stock:</ProductInfoKey>
+                            <ProductInfoValue>true/false</ProductInfoValue>
+                        </ProductInfoItem>
                     </ProductInfoBottom>
                 </ProductTopRight>
             </ProductTop>
             <ProductBottom>
-
+                <ProductForm>
+                    <ProductFormLeft>
+                        <Label>Product Name</Label>
+                        <Input type="text" placeholder="product" />
+                        <Label>Product Description</Label>
+                        <Input type="text" placeholder="desc" />
+                        <Label>Price</Label>
+                        <Input type="text" placeholder="price" />
+                        <Label>In Stock</Label>
+                        <Select name="inStock" id="idStock">
+                            <Option value="true">Yes</Option>
+                            <Option value="false">No</Option>
+                        </Select>
+                    </ProductFormLeft>
+                    <ProductFormRight>
+                        <ProductUpload>
+                            <ProductUploadImg />
+                            <Label for="file">
+                                <Publish />
+                            </Label>
+                            <Input type="file" id="file" style={{ display: "none" }} />
+                        </ProductUpload>
+                        <ProductButton>Update</ProductButton>
+                    </ProductFormRight>
+                </ProductForm>
             </ProductBottom>
         </Container>
     )
